4c3338f351d89c3dba388dde0e7f7264ce40409c
shailajanimmagari7 <shailaja.nimmagari@everest.engineering>
2024-12-16 13:35:23 +0530

    Update the index file with user router

--- index.ts
+++ index.ts
@@ -1,16 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-const app=express();
-import verifyDatabaseConnection from './dbconnection/databaseConnection';
+import express from "express";
+import cors from "cors";
+import { verifyDatabaseConnection } from "./src/dbconnection/databaseConnection";
+import UserRouter from "./src/routes/userRoutes/userRoutes";
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
-verifyDatabaseConnection();
-function startServer() {
+app.use('/api/user', UserRouter); 
+const startServer = async () => {
+  try {
+    await verifyDatabaseConnection();
     app.listen(5001, () => {
-      console.log('Server is running on http://localhost:5001');
+      console.log("Server is running on http://localhost:5001");
     });
+  } catch (error) {
+    console.error("Error during server startup:", error);
   }
+};
+
 startServer();
