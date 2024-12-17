33369ae2703a07543ef6794ec0e13f2d1c95dd23
shailajanimmagari7 <shailaja.nimmagari@everest.engineering>
2024-12-17 11:05:15 +0530

    Update the DB connection with returns

--- dbconnection/databaseConnection.ts
+++ dbconnection/databaseConnection.ts
@@ -8,9 +8,11 @@ const verifyDatabaseConnection=async()=>{
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
+    return {success:true};
   } catch (error) {
     console.error('Unable to connect to the database:', error);
-  }
+    return { success: false, message: (error as Error).message }; 
+  } 
 }
 export default verifyDatabaseConnection;
 
